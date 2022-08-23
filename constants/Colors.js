40b20a6ed297cc6fc62f536ecd3085ebc64a4725
Sai Hein Min Latt <heinminlatt.dev1@gmail.com>
2022-08-23 11:24:37 +0630

    Initial Commit

---
+++ constants/Colors.js
@@ -0,0 +1,13 @@
+export default {
+    white: '#fff',
+    black: 'black',
+    green: '#006700',
+    theme: '#f1c232',
+    primary: '#e8c919',
+    dimGray: '#696969',
+    lightGray: '#9b9b9b',
+    dryBlack: '#464646',
+    lightCreamBackground : '#cfe0f0',
+    skyBlue: '#0080ff',
+    blueMark: '#3232ff',
+}
